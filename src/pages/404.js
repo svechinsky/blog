5a0f36a51a444eb3e9da97a44b3a7c53999a0bd1
Yoni <svechinskyy@gmail.com>
2019-06-10 00:54:48 +0100

    404 update

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>Prehaps you're trying to read something that is yet to be written?</p>
   </Layout>
 )
 
